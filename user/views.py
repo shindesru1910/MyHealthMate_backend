943c28c18e64e81982642a5c9839d13232e9c7a3
shindesru1910 <119733618+shindesru1910@users.noreply.github.com>
2024-07-03 16:16:46 +0530

    urls, enter data in database using jupiter notebook

--- user/views.py
+++ user/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.http import JsonResponse
 
-# Create your views here.
+
+@api_view(['GET', 'POST'])
+def dashboard(request):
+    return JsonResponse(request, 'user/dashboard.html')
