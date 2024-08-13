20b824d60e16d32ff8afd8d4b7704b7ac08d8815
Shagufta Saiyed <shaguftasaiyed25@gmail.com>
2024-08-13 16:11:49 +0530

    changes

--- MyHealthMate_backend/settings.py
+++ MyHealthMate_backend/settings.py
@@ -126,11 +126,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LOGIN_URL = '/login/'
 
-# For user's uploaded files
-# Base URL to serve media files
-MEDIA_URL = '/media/'
-# Directory where uploaded files will be stored
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
